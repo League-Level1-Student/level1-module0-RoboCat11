1f68433ab1e23e82acdb2d527d9e0d4ba9042648
league <league@imac-20.attlocal.net>
2025-05-22 16:56:21 -0700

    Today I started Level 1 and did Houses.

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,11 +1,16 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
  */
 public class Houses {
 		
-	public void run() {
+public static void main (String [] args) {
 		// Check the recipe to find out what code to put here
-	}
+Robot Houses = new Robot();
+setRobot(155,50,5555);
 }
+	}
